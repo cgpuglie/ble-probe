03c18b16f5f4d425f7bbcd6b4ff75a033be156d0
Cody Pugliese <cody.g.pugliese@gmail.com>
2022-12-21 19:36:54 -0700

    feat: improved retry logic, added config
    Added simple config, logging libraries
    Implemented retry and error handling
    
    State: successfully connects and calls hander on change, doesn't handle
    reconnect

---
+++ config.js
@@ -0,0 +1,16 @@
+function nothing(val) {
+    return val
+}
+
+function getWithDefault(varName, def, parse = nothing) {
+    return parse(process.env[varName]) || def
+}
+
+module.exports = {
+    logLevel: getWithDefault('LOG_LEVEL', 'INFO'),
+    initialSleep: getWithDefault('INITIAL_SLEEP_MS', 500, Number),
+    loopSleep: getWithDefault('LOOP_SLEEP_MS', 30000, Number),
+    serviceId: getWithDefault('SERVICE_ID', '18424398-7cbc-11e9-8f9e-2a86e4085a59'),
+    characteristicId: getWithDefault('CHARACTERISTIC_ID', '772ae377-b3d2-ff8e-1042-5481d1e03456'),
+    deviceAliasPattern: getWithDefault('DEVICE_ALIAS_PATTERN', 'BBQ')
+}
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { createBluetooth } = require('node-ble')
 const { bluetooth, destroy } = createBluetooth()
+const { 
+    logLevel, initialSleep, loopSleep,
+    serviceId, characteristicId,
+    deviceAliasPattern
+} = require('./config.js')
 
-const serviceId = "18424398-7cbc-11e9-8f9e-2a86e4085a59"
-const characteristicId = "772ae377-b3d2-ff8e-1042-5481d1e03456"
+const log = require('loglevel')
+log.setLevel(logLevel)
+
+const pattern = new RegExp(deviceAliasPattern)
 
 async function timeOut(interval) {
     return new Promise((resolve) => {
@@ -12,73 +19,106 @@ async function timeOut(interval) {
     })
 }
 
+function format(any) {
+    let base = { timestamp: (new Date()).toISOString(), message: "" }
+    return JSON.stringify(Object.assign({}, any, base))
+}
+
+let skip = {}
 async function main() {
     const adapter = await bluetooth.defaultAdapter()
 
-    console.log("Starting discovery")
+    // Start discovery
+    log.info(format({ message: "starting discovery" }))
     if (! await adapter.isDiscovering())
         await adapter.startDiscovery()
 
+    // Handle ctrl+c
+    process.on('SIGINT', function() {
+        log.info(format({ message: "caught SIGINT, shutting down..." }))
+        adapter.stopDiscovery().then(() => {
+            destroy()
+            process.exit();
+        })
+    });
 
-    console.log("Waiting...")
-    await timeOut(6000)
+    // wait for devices
+    log.trace(format({ sleep: initialSleep }))
+    await timeOut(initialSleep)
 
-    console.log("Listing devices")
     const deviceIds = await adapter.devices()
-    console.log(JSON.stringify(deviceIds, null, 2))
+    log.trace(format({ discovered: deviceIds }))
 
-    console.log("Finding probes")
-    let probeIds = []
-    let devices = []
-    for (let i = 0; i < deviceIds.length; i++) {
-        const deviceId = deviceIds[i]
+    // Continuously attempt connections
+    while (true) {
+        for (let i = 0; i < deviceIds.length; i++) {
+            if (Object.keys(skip).length == deviceIds.length) {
+                // all IDs are either connected or don't match
+                break
+            }
 
-        const device = await adapter.getDevice(deviceId)
-        const alias = await device.getAlias()
+            const id = deviceIds[i]
+            log.trace(format({checking: id, index: i}))
 
-        if (alias == "BBQ") {
-            probeIds.push(await device.getAddress())
-            devices.push(device)
+            // skip if marked as connected or ignored
+            if (skip[id]) {
+                log.trace(format({ skipped: true, id: id }))
+                continue
+            }
+
+            let device
+            let alias
+            try {
+                device = await adapter.getDevice(id)
+                alias = await device.getAlias()
+            } catch (e) {
+                log.error({message: `Failed to get device info for '${id}'`, error: e})
+                continue
+            }
+
+            // skip if device is not a bbq probe
+            if (!alias.match(pattern)) {
+                skip[id] = true
+                log.trace(format({ skipped: true, alias }))
+                continue
+            }
+
+            log.info(format({ connecting: id }))
+            // create events
+            await device.on('connected', (_) => {
+                log.info(format({connected: id}))
+            })
+            await device.on('disconnect', (_) => {
+                log.info(format({ disconnected: id }))
+            })
+
+            try {
+                await device.connect()
+                // mark as connected
+                skip[id] = true
+
+                const gatt = await device.gatt()
+                const service = await gatt.getPrimaryService(serviceId)
+                const characteristic = await service.getCharacteristic(characteristicId)
+
+                await characteristic.startNotifications()
+                characteristic.on('valuechanged', createHandler(id))
+            }
+            catch (e) {
+                log.error({message: `connection attempt to device '${id}' failed.`, error: e})
+                continue
+            }
         }
+        
+        log.trace(format({ sleep: loopSleep }))
+        await timeOut(loopSleep)
     }
-    probeIds = probeIds.sort()
-
-    console.log(`Found: "${JSON.stringify(probeIds)}"`)
-
-    for (let i = 0; i < devices.length; i++) {
-        const device = devices[i]
-        await device.connect()
-
-        const gatt = await device.gatt()
-        const service = await gatt.getPrimaryService(serviceId)
-        const characteristic = await service.getCharacteristic(characteristicId)
-
-        // create events
-        await device.on('connected', (connected) => {
-            console.log(`${probeIds[i]} connected`)
-        })
-        await device.on('disconnect', (connected) => {
-            console.log(`${probeIds[i]} disconnected`)
-        })
-        await characteristic.startNotifications()
-        characteristic.on('valuechanged', buffer => {
-            console.log(`${probeIds[i]} value: ${buffer.toString()}`)
-        })
-        console.log('events created')
-    }
-
-    console.log('Waiting...')
-    await timeOut(120000)
-
-    for (let i = 0; i < devices.length; i++) {
-        const device = devices[i]
-
-        await device.disconnect()
-    }
-
-    console.log("Shutting down")
-    await adapter.stopDiscovery()
-    destroy()
 }
 
-main().then(console.log).catch(console.log)
+function createHandler(id) {
+    return function handler(buffer) {
+        log.info({id, value: buffer.toString()})
+    }
+}
+
+main().then(console.log).catch(console.error)
